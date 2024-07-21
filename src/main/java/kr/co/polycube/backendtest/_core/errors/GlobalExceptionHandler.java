23daa2eb5a31e5e6cee6249d0d160a022740e539
jiyoung <ij0512@naver.com>
2024-07-21 14:27:01 +0900

    errors, handler 구현

---
+++ src/main/java/kr/co/polycube/backendtest/_core/errors/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package kr.co.polycube.backendtest._core.errors;
+
+
+import kr.co.polycube.backendtest._core.errors.exception.ErrorResponse;
+import kr.co.polycube.backendtest._core.errors.exception.Exception400;
+import kr.co.polycube.backendtest._core.errors.exception.Exception404;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @org.springframework.web.bind.annotation.ExceptionHandler(Exception400.class)
+    public ResponseEntity<ErrorResponse> ex400(Exception400 e){
+        ErrorResponse responseDTO = new ErrorResponse(e.getMessage());
+        log.warn(e.getMessage());
+        return new ResponseEntity<>(responseDTO, HttpStatus.BAD_REQUEST); // http body, http header
+    }
+    @org.springframework.web.bind.annotation.ExceptionHandler(Exception404.class)
+    public ResponseEntity<ErrorResponse> ex404(Exception404 e){
+        ErrorResponse responseDTO = new ErrorResponse(e.getMessage());
+        log.warn(e.getMessage());
+        return new ResponseEntity<>(responseDTO, HttpStatus.NOT_FOUND);
+    }
+}
