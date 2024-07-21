5f1b6f0f7d6d18c35d46c57b3d0c9b18e22c8c6e
jiyoung <ij0512@naver.com>
2024-07-21 14:36:39 +0900

    user save 완료

--- src/main/java/kr/co/polycube/backendtest/user/UserController.java
+++ src/main/java/kr/co/polycube/backendtest/user/UserController.java
@@ -1,6 +1,9 @@
 package kr.co.polycube.backendtest.user;
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
@@ -8,6 +11,14 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     private final UserService userService;
+
+    @PostMapping("/users")
+    public ResponseEntity<?> saveUser(@Validated @RequestBody UserRequest.SaveUserDTO requestDTO, Errors errors){
+
+        UserResponse.SaveUserDTO responseDTO=userService.saveUser(requestDTO);
+
+        return ResponseEntity.ok(responseDTO);
+    }
 
 
 }
--- src/main/java/kr/co/polycube/backendtest/user/UserRequest.java
+++ src/main/java/kr/co/polycube/backendtest/user/UserRequest.java
@@ -1,7 +1,20 @@
 package kr.co.polycube.backendtest.user;
 
 
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
+
 public class UserRequest {
 
+    @Data
+    public static class SaveUserDTO{
+        @NotEmpty
+        private String name;
 
+        public User toEntity(){
+            return User.builder()
+                    .name(name)
+                    .build();
+        }
+    }
 }
